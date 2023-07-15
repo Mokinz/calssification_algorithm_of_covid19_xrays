e9ffbb244bd97234aad0a15a274ca2ebcd7c9602
Mokinz <jz.jakubzolich@gmail.com>
2023-07-15 10:34:05 +0200

    Newine

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import DataLoader, Dataset, random_split
+import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader, random_split
 from torchvision import transforms, datasets
+from sklearn.metrics import f1_score
+
 
 # 1. Definicja modelu sieci neuronowej
 class CovidClassifier(nn.Module):
@@ -58,21 +61,58 @@ optimizer = optim.Adam(model.parameters(), lr=0.001)
 
 # 4. Trenowanie modelu
 num_epochs = 10
+train_losses = []
+test_losses = []
+
+print("GPU ENEABLED:", torch.cuda.is_available())
+
 
 for epoch in range(num_epochs):
+    train_loss = 0
+    test_loss = 0
+
+    model.train()  # Ustawienie modelu na tryb treningowy
     for batch_idx, (data, targets) in enumerate(train_loader):
         data, targets = data.to(device), targets.to(device)
 
         # Forward pass
         scores = model(data)
         loss = criterion(scores, targets)
+        train_loss += loss.item()
 
         # Backward pass
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
-    print(f"Epoch [{epoch + 1}/{num_epochs}], Loss: {loss.item():.4f}")
+    model.eval()  # Ustawienie modelu na tryb ewaluacji
+    with torch.no_grad():
+        for batch_idx, (data, targets) in enumerate(test_loader):
+            data, targets = data.to(device), targets.to(device)
+            outputs = model(data)
+            loss = criterion(outputs, targets)
+            test_loss += loss.item()
+
+    train_loss /= len(train_loader)
+    test_loss /= len(test_loader)
+
+    train_losses.append(train_loss)
+    test_losses.append(test_loss)
+
+    print(
+        f"Epoch [{epoch + 1}/{num_epochs}], Train Loss: {train_loss:.4f}, Test Loss: {test_loss:.4f}"
+    )
+
+# Rysowanie wykresu strat
+plt.figure(figsize=(10, 5))
+plt.plot(train_losses, label="Train Loss")
+plt.plot(test_losses, label="Test Loss")
+plt.title("Loss per epoch")
+plt.xlabel("Epochs")
+plt.ylabel("Loss")
+plt.legend()
+plt.show()
+
 
 # 5. Zapisanie nauczonego modelu
 torch.save(model.state_dict(), "covid_classifier.pt")
@@ -84,6 +124,8 @@ model.eval()
 # 7. Testowanie modelu na danych testowych
 correct = 0
 total = 0
+all_labels = []
+all_predictions = []
 
 with torch.no_grad():
     for images, labels in test_loader:
@@ -93,4 +135,12 @@ with torch.no_grad():
         total += labels.size(0)
         correct += (predicted == labels).sum().item()
 
+        # Przechowujemy prawdziwe etykiety i przewidziane etykiety, aby obliczyć F1 score
+        all_labels.extend(labels.detach().cpu().numpy())
+        all_predictions.extend(predicted.detach().cpu().numpy())
+
 print(f"Test Accuracy of the model on test images: {100 * correct / total}%")
+
+# Obliczanie F1 score
+f1 = f1_score(all_labels, all_predictions, average="weighted")
+print(f"F1 Score of the model on test images: {f1}")
